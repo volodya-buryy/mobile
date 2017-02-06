509dfc0ea9607b01bf38fcdac9be87d42319d2f2
volodya-buryy <vova.buryy@yandex.com>
2017-02-06 23:13:51 +0200

    added basic socket staff

--- src/pages/main/chat/chat.ts
+++ src/pages/main/chat/chat.ts
@@ -1,9 +1,11 @@
 import { NavController } from 'ionic-angular';
 import { Component, Injectable } from '@angular/core';
+import {Router} from '@angular2/router';
 import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import * as io from "socket.io-client";
 
 @Component({
   selector: 'page-hello-ionic',
@@ -11,5 +13,20 @@ import 'rxjs/add/operator/catch';
 })
 @Injectable()
 export class Chat {    
-    
+	message = "";
+	socket:any = null;
+	constructor(){
+		this.socket = io('http://localhost:3000');
+	}
+	
+	ngOnInit() {
+		console.log('Socket');      
+		this.socket.emit('sendMessage', {content:'it works !'});  
+		// this.socket.on('chatUpdate', function() {
+		// 	this.socket.emit('newMessage', {
+		// 		'userName': "Hello",
+		// 		'text': "World"
+		// 	});
+		// });
+	}
 }
